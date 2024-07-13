75f09ccca6159ce5f276810347149ed29726ef37
Chaitanya Pandit <mohitchaitanya@gmail.com>
2024-07-13 21:31:42 +0530

    First  mit

---
+++ JavaVirtualThreads/src/Main.java
@@ -0,0 +1,36 @@
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        getTotalTimeUsingVirtualThreadPool();
+        getTotalTimeUsingCachedThreadPool();
+    }
+
+    private static void getTotalTimeUsingVirtualThreadPool(){
+        var begin = Instant.now();
+        try(ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()){
+            IntStream.range(0,100_000).forEach(task-> executor.submit(()->{
+                Thread.sleep(Duration.ofSeconds(1));
+                return task;
+            }));
+        }
+        var end = Instant.now();
+        System.out.println("Time Taken Using Virtual Thread Pools: " + Duration.between(begin,end));
+    }
+
+    private static void getTotalTimeUsingCachedThreadPool(){
+        var begin = Instant.now();
+        try(ExecutorService executor = Executors.newCachedThreadPool()){
+            IntStream.range(0,100_000).forEach(task-> executor.submit(()->{
+                Thread.sleep(Duration.ofSeconds(1));
+                return task;
+            }));
+        }
+        var end = Instant.now();
+        System.out.println("Time Taken Using Cached Thread Pool: " + Duration.between(begin,end));
+    }
+}
